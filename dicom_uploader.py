5555ad0521a3e63806218add2b362bd800782140
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2021-04-23 13:26:33 +0000

    add dicom_uploader

--- dicom_uploader.py
+++ dicom_uploader.py
@@ -4,34 +4,40 @@ import os.path
 import httplib2
 import base64
 from pydicom import dcmread
-#if len(sys.argv) != 6:
-#    print("Usage: %s [hostname] [HTTP port] [path] [username] [password] ex) %s 127.0.0.1 8042 . admin password" % (sys.argv[0], sys.argv[0]))
-#    sys.exit(0)
 
 if len(sys.argv) != 4:
-    print("Usage: python %s [path] [username] [password]\n  ex ) python %s . admin password" % (sys.argv[0], sys.argv[0]))
+    print("Usage: python %s [path] [username] [password]" % (
+        sys.argv[0]))
     sys.exit(0)
 
-URL = 'http://%s:%d/instances' % ('127.0.0.1', 8042)
+HOST_URL = '127.0.0.1'
+ORTHANC_PORT = 8042
+ORTHANC_URL = 'http://%s:%d/instances' % (HOST_URL, ORTHANC_PORT)
+SUCCESS_COUNT = 0
+TOTAL_COUNT = 0
+FAILURE_PATH = []
 
-success_count = 0
-total_file_count = 0
 
-# Upload a single file to Orthanc through the REST API
-def UploadFile(path):
-    global success_count
-    global total_file_count
+def UploadFile(dicom_path: str):
+    """[Upload single instance of DICOM image]
 
-    with open(path, 'rb') as f:
-        content = f.read()
-    total_file_count += 1
+    Args:
+        dicom_path ([str]): [path to the single DICOM image]
+    """
 
-    try:
-        sys.stdout.write("Importing %s" % path)
-        h = httplib2.Http()
-        headers = { 'content-type' : 'application/dicom' }
+    global SUCCESS_COUNT
+    global TOTAL_COUNT
 
-        if len(sys.argv) == 4:
+    with open(dicom_path, 'rb') as dicom_image:
+        content = dicom_image.read()
+        TOTAL_COUNT += 1
+
+        try:
+            sys.stdout.write("Importing %s" % dicom_path)
+
+            h = httplib2.Http()
+            headers = {'content-type': 'application/dicom'}
+
             username = sys.argv[2]
             password = sys.argv[3]
             creds_str = username + ':' + password
@@ -39,47 +45,57 @@ def UploadFile(path):
             creds_str_bytes_b64 = b'Basic ' + base64.b64encode(creds_str_bytes)
             headers['authorization'] = creds_str_bytes_b64.decode("ascii")
 
-        response, content = h.request(URL, 'POST', body = content, headers = headers)
+            response, content = h.request(
+                ORTHANC_URL, 'POST', body=content, headers=headers)
 
-        if response.status == 200:
-            sys.stdout.write(" => success\n")
-            success_count += 1
-        else:
-            sys.stdout.write(" => failure\n")
-
-    except:
-        type, value, traceback = sys.exc_info()
-        sys.stderr.write(str(value))
-        sys.stdout.write(" => unable to connect\n")
-
-def construct_subj_id(dirname):
-    return "TE" + "600" + dirname
-
-# Recursively upload dcm files in a directory
-for root, dirs, files in os.walk(sys.argv[1]):
-    for f in files:
-        ext = f.split('.')[-1]
-        if ext == 'dcm' or ext == '':
-          path = os.path.join(root, f)
-          try:
-            ds = dcmread(path)
-            subj_id = os.path.basename(os.path.dirname(os.path.dirname(path)))
-            ds.PatientID = ds.PatientName = subj_id
-            parent_dirname = os.path.basename(os.path.dirname(path))
-            if parent_dirname == 'Axial':
-              accession_number = subj_id + '_' + parent_dirname
-            elif parent_dirname == 'Coronal':
-              accession_number = subj_id + '_' + parent_dirname
+            if response.status == 200:
+                sys.stdout.write(" => success\n")
+                SUCCESS_COUNT += 1
             else:
-              print('Wrong parent_dirname!!!!!!!!!!!!!!!!!!!!!!')
-            ds.AccessionNumber = accession_number
-            #print(subj_id, accession_number)
-            ds.save_as(path)
-            UploadFile(path)
-          except Error:
-            raise Error
+                sys.stdout.write(" => failure\n")
+                FAILURE_PATH.append(dicom_path)
 
-if success_count == total_file_count:
-    print("\nSummary: all %d DICOM file(s) have been imported successfully" % success_count)
-else:
-    print("\nSummary: %d out of %d files have been imported successfully as DICOM instances" % (success_count, total_file_count))
+        except:
+            type, value, traceback = sys.exc_info()
+            sys.stderr.write(str(value))
+            sys.stdout.write(" => unable to connect\n")
+
+
+if __name__ == '__main__':
+    # Recursively upload dcm files in a directory
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for file in files:
+            file_extension = file.split('.')[-1]
+            if file_extension == 'dcm' or file_extension == '':
+                dicom_path = os.path.join(root, file)
+
+                try:
+                    subj_id = os.path.basename(
+                        os.path.dirname(os.path.dirname(dicom_path)))
+                    parent_dirname = os.path.basename(
+                        os.path.dirname(dicom_path))
+                    print(parent_dirname)
+                    # if parent_dirname == 'Axial':
+                    #     accession_number = subj_id + '_' + parent_dirname
+                    # elif parent_dirname == 'Coronal':
+                    #     accession_number = subj_id + '_' + parent_dirname
+                    # else:
+                    #     print('Wrong parent_dirname')
+                    # # print(subj_id, accession_number)
+                    # dicom_instance = dcmread(dicom_path)
+                    # dicom_instance.PatientID = dicom_instance.PatientName = subj_id
+                    # dicom_instance.AccessionNumber = accession_number
+                    # dicom_instance.save_as(dicom_path)
+                    UploadFile(dicom_path)
+                except OSError as err:
+                    sys.stdout.write("OS error: {0}".format(err))
+                except:
+                    sys.stdout.write("Unexpected error:", sys.exc_info()[0])
+
+    if SUCCESS_COUNT == TOTAL_COUNT:
+        print("\nSummary: all %d DICOM file(s) have been imported successfully" %
+              SUCCESS_COUNT)
+    else:
+        print("\nSummary: %d out of %d files have been imported successfully as DICOM instances" % (
+            SUCCESS_COUNT, TOTAL_COUNT))
+        print("\n", FAILURE_PATH)
