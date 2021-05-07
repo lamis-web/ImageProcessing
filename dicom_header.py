751149e0918a8e80988a72da2cb90b00ae6b72b8
Dongha Kang <dkang0602@gmail.com>
2021-05-07 17:21:30 +0900

    fix minor issue

--- dicom_header.py
+++ dicom_header.py
@@ -12,21 +12,29 @@ def is_dicom(file):
 # MAIN
 if __name__ == '__main__':
     directory = sys.argv[1]
-    print("===================================================")
 
-    patient_id       = input('PATIENT ID/NAME  : ')
-    accession_number = input('ACCESSION NUMBER : ')
+    if len(sys.argv) > 2:
+        print("===================================================")
 
+        ds = dcmread(directory)  # 만약 두번째 argument에 무언가가 있다면, 정보만 보여준다.
+        print(ds)        
 
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if (is_dicom(file)):
-                ds = dcmread(os.path.join(root, file))  
-                ds['PatientID'].value = patient_id
-                ds['PatientName'].value = patient_id
-                ds['AccessionNumber'].value = accession_number
-                print(os.path.join(root, file) + ' ==> Overwriting ... ')
-            else:
-                print(os.path.join(root, file) + ' ==> Non dicom ... ')
+    else:
+        print("===================================================")
+
+        patient_id       = input('PATIENT ID/NAME  : ')
+        accession_number = input('ACCESSION NUMBER : ')
+        
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if (is_dicom(file)):
+                    ds = dcmread(os.path.join(root, file))  
+                    ds['PatientID'].value = patient_id
+                    ds['PatientName'].value = patient_id
+                    ds['AccessionNumber'].value = accession_number
+                    ds.save_as(os.path.join(root, file))
+                    print(os.path.join(root, file) + ' ==> Overwriting ... ')
+                else:
+                    print(os.path.join(root, file) + ' ==> Non dicom ... ')
 
     
