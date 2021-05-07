abd7cceb5a9801d03b2402dfc4a1119307d84017
Taewon Kim <rlaxodnjs199@gmail.com>
2021-05-07 09:50:49 -0500

    update dicom header list

--- dicom_deidentifier.py
+++ dicom_deidentifier.py
@@ -35,9 +35,63 @@ dicom_paths = [os.path.join(out_dicom_folder_path,dcm) for dcm in os.listdir(out
 for dicom_path in dicom_paths:
     dicom_instance = dcmread(dicom_path)
     dicom_instance.PatientName = subj_ID
+    
     # Change all metadata here #
-
+    # dicom_instance.PatientID
+    # dicom_instance.AccessionNumber
     # dicom_instance.StudyInstanceUID
+    # dicom_instance.SeriesInstanceUID
+    # dicom_instance.SOPInstanceUID
+    # dicom_instance.StudyDescription
     # dicom_instance.SeriesDescription
+    
+    # Sanitize all metadata here #
+    # dicom_instance.StudyDate
+    # dicom_instance.SeriesDate
+    # dicom_instance.AcquisitionDate
+    # dicom_instance.ContentDate
+    # dicom_instance.OverlayDate
+    # dicom_instance.CurveDate
+    # dicom_instance.AcquisitionDatetime
+    # dicom_instance.StudyTime
+    # dicom_instance.SeriesTime
+    # dicom_instance.AcquisitionTime
+    # dicom_instance.ContentTime
+    # dicom_instance.OverlayTime
+    # dicom_instance.CurveTime
+    # dicom_instance.InstitutionName
+    # dicom_instance.InstitutionAddress
+    # dicom_instance.ReferringPhysiciansName
+    # dicom_instance.ReferringPhysiciansAddress
+    # dicom_instance.ReferringPhysiciansTelephoneNumber
+    # dicom_instance.ReferringPhysicianIDSequence
+    # dicom_instance.InstitutionalDepartmentName
+    # dicom_instance.PhysicianOfRecord
+    # dicom_instance.PhysicianOfRecordIDSequence
+    # dicom_instance.PerformingPhysiciansName
+    # dicom_instance.PerformingPhysicianIDSequence
+    # dicom_instance.NameOfPhysicianReadingStudy
+    # dicom_instance.PhysicianReadingStudyIDSequence
+    # dicom_instance.OperatorsName
+    # dicom_instance.IssuerOfPatientID
+    # dicom_instance.PatientBirthDate
+    # dicom_instance.PatientBirthTime
+    # dicom_instance.PatientSex
+    # dicom_instance.OtherPatientIDs
+    # dicom_instance.OtherPatientNames
+    # dicom_instance.PatientsBirthName
+    # dicom_instance.PatientAge
+    # dicom_instance.PatientAddress
+    # dicom_instance.PatientMothersBirthName
+    # dicom_instance.CountryOfResidence
+    # dicom_instance.RegionOfResidence
+    # dicom_instance.PatientTelephoneNumbers
+    # dicom_instance.StudyID
+    # dicom_instance.CurrentPatientLocation
+    # dicom_instance.PatientInstitutionResidence
+    # dicom_instance.DateTime
+    # dicom_instance.Date
+    # dicom_instance.Time
+    # dicom_instance.PersonName
 
     dicom_instance.save_as(dicom_path)
