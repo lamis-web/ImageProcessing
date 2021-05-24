9478d3df02a1995da0af2760cb70cba428393352
Taewon Kim <rlaxodnjs199@gmail.com>
2021-05-23 22:26:32 -0500

    update

--- dicom_csv_generator.py
+++ dicom_csv_generator.py
@@ -8,42 +8,96 @@ import shutil
 import logging
 
 src_dicom_path = str(sys.argv[1])
+# { SeriesInstanceUID : { Series Data } }
+dicom_series = {}
+# { Dicom Folder Name : Dicom Path With Min Slice Thickness }
+dicom_series_of_interest = {}
 
-dicom_series_dict = {}
 
 def is_dicom(file):
     file_extension = file.split('.')[-1]
     return file_extension.lower() == 'dcm'
 
-def extract_dicom_header_data(
-    dicom_path, mrn, ct_date):
-    dicom_img = dcmread(dicom_path)
-    series_uid = dicom_img.SeriesInstanceUID
 
-    if series_uid not in dicom_series_dict:
-        dicom_series_dict[series_uid] = {}
-        dicom_series_dict[series_uid]['patient_name'] = dicom_img.PatientName
-        dicom_series_dict[series_uid]['mrn'] = mrn
-        dicom_series_dict[series_uid]['study_description'] = dicom_img.StudyDescription
-        dicom_series_dict[series_uid]['study_date'] = ct_date
-        dicom_series_dict[series_uid]['number_of_slices'] = 0
-        dicom_series_dict[series_uid]['slice_thickness'] = dicom_img.SliceThickness
+def extract_dicom_header_data(
+        dicom_path, mrn, ct_date):
+    dicom_img = dcmread(dicom_path)
+    if dicom_img.get('SeriesInstanceUID'):
+        series_uid = dicom_img.SeriesInstanceUID
     else:
-        dicom_series_dict[series_uid]['number_of_slices'] += 1
-    
+        print("No SeriesInstanceUID")
+        return
+
+    if series_uid not in dicom_series:
+        dicom_series[series_uid] = {}
+        if dicom_img.get('PatientName'):
+            dicom_series[series_uid]['patient_name'] = dicom_img.PatientName
+        else:
+            dicom_series[series_uid]['patient_name'] = 'None'
+        if dicom_img.get('PatientID'):
+            dicom_series[series_uid]['patient_id'] = dicom_img.PatientName
+        else:
+            dicom_series[series_uid]['patinet_id'] = "None"
+        if dicom_img.get('StudyDescription'):
+            dicom_series[series_uid]['study_description'] = dicom_img.StudyDescription
+        else:
+            dicom_series[series_uid]['study_description'] = "NONE"
+        if dicom_img.get('SliceThickness'):
+            dicom_series[series_uid]['slice_thickness'] = dicom_img.SliceThickness
+            dicom_folder_name = os.path.basename(os.path.dirname(dicom_path))
+            if dicom_folder_name in dicom_series_of_interest:
+                min_thickness_dicom = dicom_series_of_interest[dicom_folder_name]
+                if min_thickness_dicom[0] > dicom_img.SliceThickness:
+                    min_thickness_dicom = [
+                        dicom_img.SliceThickness, series_uid, [dicom_path]]
+                elif min_thickness_dicom[0] == dicom_img.SliceThickness and min_thickness_dicom[1] == series_uid:
+                    min_thickness_dicom[2].append(
+                        dicom_path)
+            else:
+                dicom_series_of_interest[dicom_folder_name] = [
+                    dicom_img.SliceThickness, series_uid, [dicom_path]]
+        else:
+            dicom_series[series_uid]['slice_thickness'] = "NONE"
+        dicom_series[series_uid]['mrn'] = mrn
+        dicom_series[series_uid]['study_date'] = ct_date
+        dicom_series[series_uid]['number_of_slices'] = 0
+    else:
+        dicom_series[series_uid]['number_of_slices'] += 1
+
+
+def export_dicom_series_with_min_slice_thickness(output_path, dicom_series_of_interest):
+    output_root_path = output_path + '/dicom_min_slice_thickness'
+    if not os.path.exists(output_root_path):
+        os.mkdir(output_root_path)
+    for dicom_folder_name in dicom_series_of_interest:
+        series_to_copy = dicom_series_of_interest[dicom_folder_name]
+        output_folder_path = output_root_path + '/' + dicom_folder_name
+        if not os.path.exists(output_folder_path):
+            os.mkdir(output_folder_path)
+        for dicom_slice_path in series_to_copy[2]:
+            output_slice_path = output_folder_path + \
+                '/' + os.path.basename(dicom_slice_path)
+            shutil.copyfile(dicom_slice_path, output_slice_path)
+
+
 with open(src_dicom_path + '/dicom_stats.csv', 'w') as output_csv:
     for root, dirs, files in os.walk(src_dicom_path):
         for file in files:
             if is_dicom(file):
                 dicom_path = os.path.join(root, file)
-                dicom_folder_name = os.path.basename(os.path.dirname(dicom_path))
+                dicom_folder_name = os.path.basename(
+                    os.path.dirname(dicom_path))
 
                 mrn = dicom_folder_name.split('_')[0]
                 ct_date = dicom_folder_name.split('_')[1]
                 extract_dicom_header_data(dicom_path, mrn, ct_date)
 
-    csv_columns = ['patient_name', 'mrn', 'study_description', 'study_date', 'number_of_slices', 'slice_thickness']
+    csv_columns = ['patient_name', 'patient_id', 'mrn',
+                   'study_date', 'number_of_slices', 'slice_thickness', 'study_description']
     writer = csv.DictWriter(output_csv, fieldnames=csv_columns)
     writer.writeheader()
-    for series in dicom_series_dict:
-        writer.writerow(dicom_series_dict[series])
+    for series in dicom_series:
+        writer.writerow(dicom_series[series])
+
+export_dicom_series_with_min_slice_thickness(
+    src_dicom_path, dicom_series_of_interest)
--- dicom_deidentifier.py
+++ dicom_deidentifier.py
@@ -1,7 +1,7 @@
 # ##############################################################################
 # Usage: python dicom_deidentifier.py {dicom_path} {out_path} {subjID} {imgID}
 # Run Time: <10s
-# Ref: 
+# Ref:
 # ##############################################################################
 # Desc: copy dicom from dicom_path to out_path and change metadata
 # ##############################################################################
@@ -25,24 +25,26 @@ subj_ID = str(sys.argv[3])
 img_ID = str(sys.argv[4])
 
 # Create a logger
-logging.basicConfig(filename='dicom_identifier.log', level=logging.WARNING, filemode='w', format='%(levelname)s - %(message)s')
+logging.basicConfig(filename='dicom_identifier.log', level=logging.WARNING,
+                    filemode='w', format='%(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Prepare a output folder
-out_subj_path = os.path.join(output_path,f'{subj_ID}_{img_ID}')
+out_subj_path = os.path.join(output_path, f'{subj_ID}_{img_ID}')
 if not os.path.exists(out_subj_path):
     os.makedirs(out_subj_path)
 
 # Copy
-out_dicom_folder_path = os.path.join(out_subj_path,'dicom')
+out_dicom_folder_path = os.path.join(out_subj_path, 'dicom')
 if not os.path.exists(out_dicom_folder_path):
-    shutil.copytree(src_dicom_path,out_dicom_folder_path)
+    shutil.copytree(src_dicom_path, out_dicom_folder_path)
 
 # Overwrite
-dicom_paths = [os.path.join(out_dicom_folder_path,dcm) for dcm in os.listdir(out_dicom_folder_path)]
+dicom_paths = [os.path.join(out_dicom_folder_path, dcm)
+               for dcm in os.listdir(out_dicom_folder_path)]
 for dicom_path in dicom_paths:
     dicom_instance = dcmread(dicom_path)
-    
+
     if dicom_instance.get('PatientName'):
         dicom_instance.PatientName = subj_ID
         logger.info(f'{dicom_path} - PatientName Header Processed')
@@ -51,28 +53,30 @@ for dicom_path in dicom_paths:
         continue
 
     if dicom_instance.get('PatientID'):
-	    dicom_instance.PatientID = subj_ID
+        dicom_instance.PatientID = subj_ID
         logger.info(f'{dicom_path} - PatientID Header Processed')
     else:
         logger.error(f'{dicom_path} - PatientID Header does not exist')
         continue
 
     if dicom_instance.get('PatientBirthDate'):
-	    dicom_instance.PatientBirthDate = str(int(dicom_instance.PatientBirthDate) + (randint(1, 500) * 10000))
+        dicom_instance.PatientBirthDate = str(
+            int(dicom_instance.PatientBirthDate) + (randint(1, 500) * 10000))
         logger.info(f'{dicom_path} - PatientBirthDate Header Processed')
     else:
         logger.error(f'{dicom_path} - PatientBirthDate Header does not exist')
         continue
-    
+
     if dicom_instance.get('AccessionNumber'):
-	    dicom_instance.AccessionNumber = subj_ID + "_" + img_ID
+        dicom_instance.AccessionNumber = subj_ID + "_" + img_ID
         logger.info(f'{dicom_path} - AccessionNumber Header Processed')
     else:
         logger.error(f'{dicom_path} - AccessionNumber Header does not exist')
         continue
 
     if dicom_instance.get('StudyDate'):
-	    dicom_instance.StudyDate = str(int(dicom_instance.StudyDate) + (randint(1, 30)))
+        dicom_instance.StudyDate = str(
+            int(dicom_instance.StudyDate) + (randint(1, 30)))
         logger.info(f'{dicom_path} - StudyDate Header Processed')
     else:
         logger.error(f'{dicom_path} - StudyDate Header does not exist')
@@ -81,20 +85,26 @@ for dicom_path in dicom_paths:
     if dicom_instance.get('SeriesDate'):
         dicom_instance.SeriesDate = dicom_instance.StudyDate
     else:
-        logger.warn(f'{dicom_path} - SeriesDate Header does not exist: Appending Header')
-        dicom_instance.add_new([0x0008, 0x0021], dictionary_VR([0x0008, 0x0021]), dicom_instance.StudyDate)
+        logger.warn(
+            f'{dicom_path} - SeriesDate Header does not exist: Appending Header')
+        dicom_instance.add_new([0x0008, 0x0021], dictionary_VR(
+            [0x0008, 0x0021]), dicom_instance.StudyDate)
 
     if dicom_instance.get('AcquisitionDate'):
         dicom_instance.AcquisitionDate = dicom_instance.StudyDate
     else:
-        logger.warn(f'{dicom_path} - AcquisitionDate Header does not exist: Appending Header')
-        dicom_instance.add_new([0x0008, 0x0022], dictionary_VR([0x0008, 0x0022]), dicom_instance.StudyDate)
+        logger.warn(
+            f'{dicom_path} - AcquisitionDate Header does not exist: Appending Header')
+        dicom_instance.add_new([0x0008, 0x0022], dictionary_VR(
+            [0x0008, 0x0022]), dicom_instance.StudyDate)
 
     if dicom_instance.get('ContentDate'):
         dicom_instance.ContentDate = dicom_instance.ContentDate
     else:
-        logger.warn(f'{dicom_path} - ContentDate Header does not exist: Appending Header')
-        dicom_instance.add_new([0x0008, 0x0023], dictionary_VR([0x0008, 0x0023]), dicom_instance.StudyDate)
+        logger.warn(
+            f'{dicom_path} - ContentDate Header does not exist: Appending Header')
+        dicom_instance.add_new([0x0008, 0x0023], dictionary_VR(
+            [0x0008, 0x0023]), dicom_instance.StudyDate)
 
     # Change all metadata here #
     # dicom_instance.StudyInstanceUID
@@ -102,7 +112,7 @@ for dicom_path in dicom_paths:
     # dicom_instance.SOPInstanceUID
     # dicom_instance.StudyDescription
     # dicom_instance.SeriesDescription
-    
+
     # Sanitize all metadata here #
     # dicom_instance.OverlayDate
     # dicom_instance.CurveDate
