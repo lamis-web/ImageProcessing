a9109601d8bf8a165a9a3c97f5a72c418f63eb32
Taewon Kim <rlaxodnjs199@gmail.com>
2021-06-30 11:06:33 -0500

    update

--- datasheet_stats.py
+++ datasheet_stats.py
@@ -1,7 +1,8 @@
+from os import lseek
 import pandas as pd
 
 START_VIDA_CASE_ID = 960
-EXCEL_PATH = './DataSheet_20210616.xlsx'
+EXCEL_PATH = './Data/Datasheet/DataSheet_20210629.xlsx'
 df = pd.read_excel(EXCEL_PATH, sheet_name=0)
 df = df[df['VidaCaseID'] >= START_VIDA_CASE_ID]
 
@@ -19,3 +20,7 @@ for _, row in df.iterrows():
 
 print(
     f'SUCCESS: {SUCCESS_COUNT}, NEED_LABELS: {NEED_LABELS_COUNT}, FAILURE: {FAILURE_COUNT}')
+
+need_to_work_df = df[(df['Vida Progress'] == 'Success')
+                     & (df['Progress'] != 'Done')]
+print(need_to_work_df['SliceThickness_mm'].value_counts())
---
+++ match_carissawalter_to_heron.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+CARISSA_WALTER_DATASHEET_PATH = './Data/CarissaWalter/COVID_CTs_20210513CarissaWalter_0608jc_20210630.xlsx'
+HERON_DATASHEET_PATH = './Data/Heron/20210629_HeronData.xlsx'
+EXCEL_OUTPUT_PATH = './Output/C19_Demographics.xlsx'
+
+carissa_df = pd.read_excel(
+    CARISSA_WALTER_DATASHEET_PATH, header=9, usecols='A,I,E,J')
+heron_df = pd.read_excel(HERON_DATASHEET_PATH, usecols='B,H,K,E,F,PY,FB,QH,PP')
+merged_df = pd.merge(carissa_df, heron_df, how="inner", on=['mrn'])
+
+merged_df.to_excel(EXCEL_OUTPUT_PATH, index=False)
