99229c1b9679a16fa8d97a1f5412f315c0bbe3bb
Taewon Kim <rlaxodnjs199@gmail.com>
2021-07-06 12:15:32 -0500

    update

--- datasheet_stats.py
+++ datasheet_stats.py
@@ -1,10 +1,11 @@
-from os import lseek
 import pandas as pd
 
 START_VIDA_CASE_ID = 960
-EXCEL_PATH = './Data/Datasheet/DataSheet_20210629.xlsx'
+END_VIDA_CASE_ID = 1394
+EXCEL_PATH = 'Data/Datasheet/DataSheet.xlsx'
 df = pd.read_excel(EXCEL_PATH, sheet_name=0)
-df = df[df['VidaCaseID'] >= START_VIDA_CASE_ID]
+df = df[(df['VidaCaseID'] >= START_VIDA_CASE_ID)
+        & (df['VidaCaseID'] <= 1394)]
 
 SUCCESS_COUNT = 0
 NEED_LABELS_COUNT = 0
@@ -21,6 +22,14 @@ for _, row in df.iterrows():
 print(
     f'SUCCESS: {SUCCESS_COUNT}, NEED_LABELS: {NEED_LABELS_COUNT}, FAILURE: {FAILURE_COUNT}')
 
-need_to_work_df = df[(df['Vida Progress'] == 'Success')
-                     & (df['Progress'] != 'Done')]
+print(df['Vida Progress'].value_counts())
+
+need_to_work_df = df[(df['Vida Progress'] == 'Success')]
 print(need_to_work_df['SliceThickness_mm'].value_counts())
+
+need_label_df = df[(df['Vida Progress'] == 'Needs Labels')]
+print(need_label_df['SliceThickness_mm'].value_counts())
+
+failure_label_df = df[(df['Vida Progress'] != 'Success')
+                      & (df['Vida Progress'] != 'Needs Labels')]
+print(failure_label_df['SliceThickness_mm'].value_counts())
